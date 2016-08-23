3f0cdaf595ad20fa0c14c16e093c130f7ba63f6f
Umair <uzafar235@gmail.com>
2016-08-23 15:02:19 +0100

    customized work

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
 
   get 'test' => "pages#test"
 
+  get 'help' => "pages#help"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
